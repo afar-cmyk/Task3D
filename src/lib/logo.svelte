92f9848b1f9ce145656369d600844ec767daea27
Andrés Alvarez <andresfelipe.alvarez03@gmail.com>
2022-08-23 02:15:30 -0500

    Modifica Svelte

---
+++ src/lib/logo.svelte
@@ -0,0 +1,26 @@
+<script>
+	import '@fontsource/dosis';
+</script>
+
+<h1 class="tituloPrueba">
+	<span class="tituloPrimero">TASK</span><span class="tituloSegundo">3D</span>
+</h1>
+
+<style>
+	h1 {
+		user-select: none;
+	}
+	.tituloPrueba {
+		font-family: 'Dosis', sans-serif;
+		font-size: 64px;
+		margin: 0;
+	}
+	.tituloPrimero {
+		font-weight: 400;
+		color: #f0f6fb;
+	}
+	.tituloSegundo {
+		font-weight: 600;
+		color: #6dbea4;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,26 @@
-<script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+<script>
+	import Logo from '../lib/logo.svelte';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<body>
+	<div class="contenedor-logo">
+		<Logo />
+	</div>
+</body>
 
 <style>
-	section {
+	body {
+		background: linear-gradient(161.15deg, #262b2c 12.73%, #181b1b 72.95%);
+		min-width: 100vw;
+		min-height: 100vh;
+		margin: 0;
+		padding: 0;
+	}
+	.contenedor-logo {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
 		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+		justify-content: center;
+		height: 100vh;
 	}
 </style>
