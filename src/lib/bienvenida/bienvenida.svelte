b1e323f1f2b2e078c01c5fd35ad11f870cc03bce
Andrés F. Alvarez <andresfelipe.alvarez03@gmail.com>
2022-08-31 01:50:09 -0500

    Ajustar el componente bienvenida

--- src/lib/bienvenida/bienvenida.svelte
+++ src/lib/bienvenida/bienvenida.svelte
@@ -1,11 +1,27 @@
 <script>
 	import Logo from '../logo.svelte';
 	import Footer from '../footer.svelte';
+	import { onMount } from 'svelte';
+
+	// Funcion que devuelve True o False si es la primera vez en el sitio
+	const comprobarPrimeraVez = () => {
+		return !localStorage.getItem('primeraVez');
+	};
+
+	// Comprueba al entrar al sitio si es la primera vez en este
+	onMount(() => {
+		comprobarPrimeraVez();
+	});
 </script>
 
-<div class="contenedor-logo">
-	<Logo />
-</div>
+{#if comprobarPrimeraVez()}
+	<div class="contenedor-logo">
+		<Logo />
+	</div>
+{:else}
+	<p>Pagina inicio descripcion y boton</p>
+{/if}
+
 <Footer />
 
 <style>
