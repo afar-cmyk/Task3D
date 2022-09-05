ebd94299a890e8717d04c940d72e563eb3ed0a5a
Andrés F. Alvarez <andresfelipe.alvarez03@gmail.com>
2022-09-05 00:52:31 -0500

    Añadir estilos al boton principal

--- src/lib/bienvenida/paginaInicial.svelte
+++ src/lib/bienvenida/paginaInicial.svelte
@@ -1,12 +1,13 @@
 <script>
 	import Logo from '../logo.svelte';
 	import BotonPrincipal from '../botonPrincipal.svelte';
+	import '@fontsource/inter';
 </script>
 
 <main>
 	<Logo --tamaño-texto="48px" logoNormal="true" />
 	<span class="texto-principal">
-		<p>¿Quieres crear una nueva rutina y no sabes por donde comenzar?</p>
+		<p class="primer-texto">¿Quieres crear una nueva rutina y no sabes por donde comenzar?</p>
 		<p>¡Los estudios revelan que solo necesitas 30 dias para comenzar una rutina!</p>
 	</span>
 
@@ -14,11 +15,27 @@
 </main>
 
 <style>
+	main {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		padding-left: 22px;
+		padding-right: 22px;
+		gap: 34px;
+	}
 	.texto-principal {
 		font-family: 'Inter', sans-serif;
 		font-weight: 200;
 		font-size: 16px;
 		line-height: 19px;
 		color: #ffffff;
+		margin: 0;
+	}
+	.texto-principal p {
+		margin-bottom: 0;
+	}
+	.primer-texto {
+		margin-top: 0;
 	}
 </style>
--- src/lib/botonPrincipal.svelte
+++ src/lib/botonPrincipal.svelte
@@ -10,7 +10,7 @@
 	.boton {
 		background: #6dbea4;
 		border-radius: 32px;
-		width: 100vw;
+		width: 100%;
 		font-family: 'Dosis';
 		font-weight: 600;
 		font-size: 18px;
@@ -18,5 +18,13 @@
 		padding: 10px 0 10px 0;
 		color: #262b2c;
 		border: none;
+	}
+	.boton:hover {
+		background: #80c6b0;
+		color: #343b3c;
+	}
+	.boton:active {
+		background: #4dac8e;
+		color: #181b1b;
 	}
 </style>
